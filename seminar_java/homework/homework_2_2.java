4430664ac4c88ae7a4493ff718e37d04e212686b
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-08-13 17:56:24 +0300

    решениe 2ой задачи

--- seminar_java/homework/homework_2_2.java
+++ seminar_java/homework/homework_2_2.java
@@ -5,32 +5,67 @@
 
 package seminar_java.homework;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class homework_2_2 {
+    static char[] sGlobal;
+    static ArrayList<Integer> singIndexesGlobal = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        String s = "2? + 1? = ??";
+        String s = "2? + ?? = 3?";
 
         int questionCount;
         questionCount = s.length() - s.replace("?", "").length();
         int k = 10;
         int[] nums = new int[questionCount];
-//        int[] numbers = generate(nums, 0, k);
-//        String equation = s.replace("?", numbers[i]);
-    }
+        ArrayList<Integer> singIndexes = new ArrayList<>();
 
+//        System.out.println(Arrays.toString(nums));
+
+        char[] equation = s.replaceAll(" ", "").toCharArray();
+        for (int i = 0; i < equation.length; i++) {
+            if(equation[i] == '+' || equation[i] == '=') {
+                equation[i] = '-';
+            }
+            if(equation[i] == '?') {
+                singIndexes.add(i);
+            }
+        }
+
+        sGlobal = equation;
+        singIndexesGlobal = singIndexes;
+
+        generate(new int[singIndexes.size()], 0, k);
+
+    }
 
     private static void generate(int[] arrays, int index, int k) {
         if (index == arrays.length) {
-            System.out.println(Arrays.toString(arrays));
+            check(arrays);
             return;
         }
 
         for (int i = 0; i < k; i++) {
             arrays[index] = i;
             generate(arrays, index + 1, k);
+        }
+    }
+    private static void check(int[] arrays) {
+       for (int i = 0; i < arrays.length; i++) {
+           sGlobal[singIndexesGlobal.get(i)] = Character.forDigit(arrays[i], 10);
+        }
+       String[] str = String.valueOf(sGlobal).split("-");
+       int a = Integer.parseInt(str[0]);
+       int b = Integer.parseInt(str[1]);
+       int c = Integer.parseInt(str[2]);
+
+       if (a + b == c) {
+            System.out.printf("%d + %d = %d", a, b, c);
+           System.out.println();
         }
     }
 }
