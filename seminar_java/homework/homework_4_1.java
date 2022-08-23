cfa789f7255f0346e50a3cacc3ab27dbca829ec4
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-08-23 23:44:24 +0300

    домашка

--- seminar_java/homework/homework_4_1.java
+++ seminar_java/homework/homework_4_1.java
@@ -5,46 +5,85 @@
 
 package seminar_java.homework;
 
+import java.util.Arrays;
+
 public class homework_4_1 {
     public static void main(String[] args) {
 
-        int [] array = {3,1,6,2,0,4};
-        sortArray(array);
+        int [] array = {33,31,26,2,16,14,22,0, -15};
 
-        for (int i = 0; i < array.length ; i++) {
-            System.out.print(array[i] + " ");
-        }
+        System.out.println(Arrays.toString(arraySort(array, 0, array.length-1)));
+
 
     }
-    public static int[] sortArray(int[] firstArray){
-        if(firstArray == null) {
-            return null;
+//    public static int[] sortArray(int[] array){
+//        if(array == null) {
+//            return null;
+//        }
+//        if(array.length < 2){
+//            return array;
+//        }
+//
+//        int [] leftArray = new int [array.length/2];
+//        System.arraycopy(array, 0, leftArray, 0, array.length/2);
+//
+//        int [] rightArray = new int [array.length - array.length/2];
+//        System.arraycopy(array, array.length/2, rightArray, 0, array.length - array.length/2);
+//
+//
+//        sortArray(leftArray);
+//        sortArray(rightArray);
+//
+//        int [] finishArray = mergeArray(leftArray, rightArray);
+//
+//        return finishArray;
+//    }
+
+    public static int[] arraySort(int[] array, int leftPointer, int rightPointer) {
+        if (leftPointer == rightPointer) {
+            return new int[]{array[leftPointer]};
         }
-        if(firstArray.length < 2){
-            return firstArray;
-        }
+        int mediumPointer = leftPointer + (rightPointer - leftPointer) / 2;
 
-        int [] secondArray = new int [firstArray.length/2];
-        System.arraycopy(firstArray, 0, secondArray, 0, firstArray.length/2);
+        int[] leftPart = arraySort(array, leftPointer, mediumPointer);
+        int[] rightPart = arraySort(array, mediumPointer + 1, rightPointer);
+        int[] finishArray = mergeArray(leftPart, rightPart);
 
-        int [] thirdArray = new int [firstArray.length - firstArray.length/2];
-        System.arraycopy(firstArray, firstArray.length/2, thirdArray, 0, firstArray.length - firstArray.length/2);
-
-        secondArray = sortArray(secondArray);
-        thirdArray = sortArray(thirdArray);
-
-        return mergeArray(secondArray, thirdArray);
+        return finishArray;
     }
 
-    public static int[] mergeArray(int[] firstArray, int[] secondArray) {
-        int [] resultArray = new int[firstArray.length + secondArray.length];
-        for (int i = 0; i < resultArray.length; i++) {
-            if((firstArray[i]) < (secondArray[i])){
-                resultArray[i] = firstArray[i];
+    public static int[] mergeArray(int[] leftArray, int[] rightArray) {
+
+        int indexLeft = 0;
+        int indexRight = 0;
+        int indexResult = 0;
+
+        int [] resultArray = new int[rightArray.length + leftArray.length];
+        while(indexLeft < leftArray.length && indexRight < rightArray.length) {
+            if(leftArray[indexLeft] < rightArray[indexRight]){
+                resultArray[indexResult] = leftArray[indexLeft];
+                indexLeft++;
+                indexResult++;
+
             }else{
-                resultArray[i] = secondArray[i];
+                resultArray[indexResult] = rightArray[indexRight];
+                indexResult++;
+                indexRight++;
             }
         }
+
+        while(indexLeft < leftArray.length) {
+            resultArray[indexResult] = leftArray[indexLeft];
+            indexLeft++;
+            indexResult++;
+        }
+
+        while(indexRight < rightArray.length) {
+            resultArray[indexResult] = rightArray[indexRight];
+            indexRight++;
+            indexResult++;
+        }
+
         return resultArray;
     }
 }
